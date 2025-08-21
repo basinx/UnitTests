f8f601438843e2cc388318a5a200358e6e2e3505
Jordan <jtaylor702@yahoo.com>
2025-08-21 15:05:38 -0600

    Add unit tests for BankAccount class functionality

---
+++ test_bank_account.py
@@ -0,0 +1,27 @@
+import unittest
+from bank_account import BankAccount
+
+class TestBankAccount(unittest.TestCase):
+    def test_initial_balance(self):
+        account = BankAccount()
+        self.assertEqual(account.balance, 0)
+
+    def test_deposit(self):
+        account = BankAccount()
+        account.deposit(100)
+        self.assertEqual(account.balance, 200)
+
+    def test_withdraw(self):
+        account = BankAccount()
+        account.deposit(100)
+        account.withdraw(50)
+        self.assertEqual(account.balance, 50)
+
+    def test_withdraw_insufficient_funds(self):
+        account = BankAccount()
+        account.deposit(50)
+        with self.assertRaises(ValueError):
+            account.withdraw(100)
+
+if __name__ == '__main__':
+    unittest.main()
